5954f5283a07fe7ed6b7f3fd3ae5def0b97b3c5e
Frank-G-M <frankmayorgaguerrero@gmail.com>
2025-04-24 16:34:36 -0500

    first commit

---
+++ ProyectOne/src/test/java/com/omswrtm/ProyectOne/ProyectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omswrtm.ProyectOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
