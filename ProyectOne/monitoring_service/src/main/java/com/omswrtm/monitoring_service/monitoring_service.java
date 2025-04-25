b9ce8340da6f8e9e459e7b81313d13d05506223c
Frank-G-M <frankmayorgaguerrero@gmail.com>
2025-04-25 18:44:42 -0500

    Creación modulos

---
+++ ProyectOne/monitoring_service/src/main/java/com/omswrtm/monitoring_service/monitoring_service.java
@@ -0,0 +1,9 @@
+package com.omswrtm.monitoring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class monitoring_service {
+    public static void main(String[]args){SpringApplication.run(monitoring_service.class,args);}
+}
---
+++ ProyectOne/service_discovery/src/main/java/com/omswrtm/service_discovery/service_discovery.java
@@ -0,0 +1,10 @@
+package com.omswrtm.service_discovery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class service_discovery {
+    public static void main(String[]args){SpringApplication.run(service_discovery.class,args);}
+}
